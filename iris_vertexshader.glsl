63e275863335ef2aa2546b7001b00e47a9f41d27
HenriPK <auralinkk@gmail.com>
2020-07-26 23:02:36 -0300

    Cleaned Directory Further

--- iris_vertexshader.glsl
+++
@@ -1,7 +0,0 @@
-attribute vec3 aVertexPosition;
-uniform float aspect_ratio;
-uniform float time;
-
-void main(void) {
-	gl_Position = vec4(sin(time) * 0.6, 0.0, 0.0, 0.0) + vec4(aVertexPosition * vec3(1.0, aspect_ratio, 1.0), 1.0);
-}
