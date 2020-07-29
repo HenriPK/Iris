49b0806e6a04381a591fc3c6dd3c17675062ec97
HenriPK <auralinkk@gmail.com>
2020-07-28 21:00:30 -0300

    Refactored, removed bug, added a cute test model!

--- game.js
+++ game.js
@@ -6,20 +6,42 @@ window.addEventListener('keyup',function(e){
     keyState[e.keyCode || e.which] = false;
 },true);
 
-x = 100;
+var mouse_pos = {};
+var mouse_uv = {};
 
+function getMousePos(canvas, evt)
+{
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
+
+function game_init()
+{
+	window.addEventListener('mousemove',function(e){
+	mouse_pos = getMousePos(viewport, e);
+	mouse_uv.x = mouse_pos.x/viewport.width - 0.5;
+	mouse_uv.y = mouse_pos.y/viewport.height - 0.5;
+}, false);
+}
 
 function update(event)
 {
 	//LEFT
 	if(keyState[122]) toggleFullScreen("canvas");
 	
-	if(keyState[37]) c_camera.position[1] += -0.1
-	if(keyState[38]) c_camera.position[0] += 0.1
-	if(keyState[39]) c_camera.position[1] += 0.1
-	if(keyState[40]) c_camera.position[0] += -0.1
-	if(keyState[32]) c_camera.position[2] += 0.1
-	if(keyState[16]) c_camera.position[2] += -0.1
+	if(keyState[37]) c_camera.position[1] += -0.7;
+	if(keyState[38]) c_camera.position[0] += 0.7;
+	if(keyState[39]) c_camera.position[1] += 0.7;
+	if(keyState[40]) c_camera.position[0] += -0.7;
+	if(keyState[32]) c_camera.position[2] += -0.7;
+	if(keyState[16]) c_camera.position[2] += 0.7;
+	
+	var angle = mouse_uv.x * 80 * Math.PI / 180;
+	var angle2 = mouse_uv.y * 80 * Math.PI / 180;
+	c_camera.rotation = [0.5 * (Math.cos(angle) + Math.cos(angle2)), -Math.sin(angle), Math.sin(angle2), 0.0];
 }
 
 function toggleFullScreen(id) {
--- iris_render.js
+++ iris_render.js
@@ -5,39 +5,83 @@ var aspect_ratio;
 var square_vertices_buffer;
 var viewport_height, viewport_width;
 
+var texture;
+
 //Current camera object!
 var c_camera = {};
-c_camera.position = [-5.0, 1.0, 1.0];
-c_camera.size = [640.0/360.0, 1.0, 1.0]
-c_camera.facing_dir = [1.0, 0.0, 0.0]
+c_camera.position = [-10.0, 6.0, 8.0];
+c_camera.size = [640.0/360.0, 1.0, 1.0];
+c_camera.rotation = [0.0, 0.0, 0.0, 0.0];
 
 //Current scene object!
 var c_scene = {};
 c_scene.clear_color = [1.0, 1.0, 1.0];
 c_scene.name = "Cena 1";
 
-var start_time = new Date();
+var start_time;
 
 var dt;
 var ts = 0;
 var gl;
 
 //The LOOP Function
-function loop(timestamp)
+function loop()
 {
-	var progress = timestamp - dt;
-	
 	update();
 	draw();
-	
-	dt = timestamp;
 	window.requestAnimationFrame(loop);
 }
 
+var viewport;
+
+function draw()
+{
+	send_uniforms();
+	
+	//gl.clearColor(0.7, 0.7, 1.0, 1.0);
+	//gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, square_vertices_buffer);
+	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+}
+
+var img = new Image();
+var model_size = [50, 50, 64];
+var k = false;
+
 function init()
 {	
-	const viewport = document.querySelector("#canvas");
+	viewport = document.querySelector("#canvas");
 	gl = viewport.getContext("webgl2");
+	
+	start_time = (new Date()).getTime();
+	
+	game_init();
+	
+	texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([128, 200, 255, 255]));
+
+	img.crossOrigin = "";
+	img.src = "assets/Oliver.png";
+	
+	img.onload = function(){	
+		{
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			
+			//document.getElementById("model").crossOrigin = "";
+			//document.getElementById("model").src = image.src;
+			//document.getElementById("model").width = 10;
+		}
+		
+		loop();
+	}
 	
 	viewport_height = viewport.height;
 	viewport_width = viewport.width;
@@ -51,13 +95,11 @@ function init()
 		return;
 	}
 	
-	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.clearColor(0.5, 0.5, 0.5, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	
 	init_shaders();
 	init_buffers();
-	
-	window.requestAnimationFrame(loop);
 }
 
 function init_shaders()
@@ -80,6 +122,40 @@ function init_shaders()
 	
 	vertexPositionAttribute = gl.getAttribLocation(shader_program, "aVertexPosition");
 	gl.enableVertexAttribArray(vertexPositionAttribute);
+}
+
+function send_uniforms() {
+	
+	shader_program.aspect_ratio = gl.getUniformLocation(shader_program, "aspect_ratio");
+	shader_program.time = gl.getUniformLocation(shader_program, "time");
+	shader_program.resolution = gl.getUniformLocation(shader_program, "resolution");
+	shader_program.clear_color = gl.getUniformLocation(shader_program, "clear_color");
+	
+	gl.uniform1f(shader_program.aspect_ratio, aspect_ratio);
+	gl.uniform1f(shader_program.time, wall_clock_time()/1000);
+	gl.uniform2fv(shader_program.resolution, [viewport_width, viewport_height]);
+	gl.uniform3fv(shader_program.clear_color, c_scene.clear_color);
+
+	//Send camera uniforms
+	shader_program.camera_position = gl.getUniformLocation(shader_program, "camera_position");
+	shader_program.camera_size = gl.getUniformLocation(shader_program, "camera_size");
+	shader_program.camera_facing_dir = gl.getUniformLocation(shader_program, "camera_rotation");
+
+	gl.uniform3fv(shader_program.camera_position, c_camera.position);
+	gl.uniform3fv(shader_program.camera_size, c_camera.size);
+	gl.uniform4fv(shader_program.camera_facing_dir, c_camera.rotation);
+	
+	//Send model uniforms
+	shader_program.model_size = gl.getUniformLocation(shader_program, "v_model_size");
+
+	gl.uniform3fv(shader_program.model_size, [50, 50, 64]);
+}
+
+//Returns the time elapsed since the beggining of time.
+function wall_clock_time()
+{
+	var current_time = (new Date()).getTime();
+	return current_time - start_time;
 }
 
 function getShader(id)
@@ -134,56 +210,10 @@ function init_buffers()
 	
 	var vertices = [
 		-1.0, -1.0, 0.0,
-		1.0, -1.0, 0.0,
-		1.0, 1.0, 0.0,
-		-1.0, 1.0, 0.0
+		 1.0, -1.0, 0.0,
+		 1.0,  1.0, 0.0,
+		-1.0,  1.0, 0.0
 	];
 	
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-}
-
-function send_uniforms() {
-	
-	shader_program.aspect_ratio = gl.getUniformLocation(shader_program, "aspect_ratio");
-	shader_program.time = gl.getUniformLocation(shader_program, "time");
-	shader_program.resolution = gl.getUniformLocation(shader_program, "resolution");
-	shader_program.clear_color = gl.getUniformLocation(shader_program, "clear_color");
-	
-	gl.uniform1f(shader_program.aspect_ratio, aspect_ratio);
-	gl.uniform1f(shader_program.time, wall_clock_time()/1000);
-	gl.uniform2fv(shader_program.resolution, [viewport_width, viewport_height]);
-	gl.uniform3fv(shader_program.clear_color, c_scene.clear_color);
-
-	//Send camera uniforms
-	shader_program.camera_position = gl.getUniformLocation(shader_program, "camera_position");
-	shader_program.camera_size = gl.getUniformLocation(shader_program, "camera_size");
-	shader_program.camera_facing_dir = gl.getUniformLocation(shader_program, "camera_facing_dir");
-
-	gl.uniform3fv(shader_program.camera_position, c_camera.position);
-	gl.uniform3fv(shader_program.camera_size, c_camera.size);
-	gl.uniform3fv(shader_program.camera_facing_dir, c_camera.facing_dir);
-	
-	//Send model uniforms
-	//shader_program.model_size = gl.getUniformLocation(shader_program, "model_size");
-
-	//gl.uniform3fv(shader_program.model_size, [4.0, 4.0, 4.0]);
-}
-
-function draw()
-{
-	send_uniforms();
-	
-	gl.clearColor(0.7, 0.7, 1.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	
-	gl.bindBuffer(gl.ARRAY_BUFFER, square_vertices_buffer);
-	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-}
-
-//Returns the time elapsed since the beggining of time.
-function wall_clock_time()
-{
-	var current_time = new Date();
-	return current_time - start_time;
 }
